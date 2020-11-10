d4e6c5fd252ef36adb021ddc9527386efa8f49b4
Chris DeLashmutt <cdelashmutt@pivotal.io>
2020-11-09 21:24:13 -0500

    Testing

--- home/workshop/config.js
+++ home/workshop/config.js
@@ -1,11 +1,10 @@
 const { exec } = require("child_process");
 
-
 function initialize(workshop) {
   workshop.load_workshop();
 
   exec("yq r ~/.kube/config 'users(name==eduk8s).user.token'", (error, stdout, stderr) => {
-    output = ""
+    output = "Wow"
     if (error) {
         output += error.message + "\n";
     }
