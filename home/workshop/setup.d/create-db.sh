4ef175bb639f55a3495a177104a66aec669d0a57
Chris DeLashmutt <cdelashmutt@pivotal.io>
2020-11-15 14:49:14 -0500

    Fix arg name

--- home/workshop/setup.d/create-db.sh
+++ home/workshop/setup.d/create-db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
-helm upgrade -i petclinic-db bitnami/mysql -v 6.14.11 -f <(cat petclinic-db-values.yaml | envsubst)
+helm upgrade -i petclinic-db bitnami/mysql --version 6.14.11 -f <(cat petclinic-db-values.yaml | envsubst)
