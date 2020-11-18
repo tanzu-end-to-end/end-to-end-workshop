22b1632be546cfca74def723f1696aa439a285e2
Chris DeLashmutt <cdelashmutt@pivotal.io>
2020-11-18 11:22:40 -0500

    Change to sync exec of process

--- home/workshop/config.js
+++ home/workshop/config.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
-const { spawn } = require('child_process');
+const { spawnSync } = require('child_process');
 
 function initialize(workshop) {
   workshop.load_workshop();
@@ -11,27 +11,17 @@ function initialize(workshop) {
   workshop.data_variable('user_token', data.users[0].user.token);
 
   let namespace = process.env['SESSION_NAMESPACE']
-  kubectl = spawn('kubectl', ['-n', namespace, 'get', 'harborproject', namespace, '-o', 'jsonpath="{.status.projectid}"']);
+  kubectl = spawnSync('kubectl', ['-n', namespace, 'get', 'harborproject', namespace, '-o', 'jsonpath="{.status.projectid}"']);
 
-  let kubectl_out = ""
-  let kubectl_err = ""
-
-  kubectl.stdout.on('data', (out) => {
-    console.log(`stdout: ${out}`);
-    kubectl_out = out;
-  });
-  
-  kubectl.stderr.on('data', (err) => {
-    console.error(`stderr: ${err}`);
-    kubectl_err = err;
-  });
-  
-  kubectl.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-    if(code == 0) {
-      workshop.data_variable('harbor_project_id', kubectl_out);
-    }
-  });
+  if(kubectl.status == 0) {
+    let project_id = kubectl.stdout;
+    console.log(`Got harbor project id: ${project_id}` );
+    workshop.data_variable('harbor_project_id', project_id);
+  } else {
+    console.error("Error getting project id.");
+    console.error(`stdout: ${kubectl.stdout}`)
+    console.error(`stderr: ${kubectl.stderr}`);
+  }
 }
 
 exports.default = initialize;
