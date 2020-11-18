6a3256d49b8646aa54e2d9900fab21c01ae095ac
Chris DeLashmutt <cdelashmutt@pivotal.io>
2020-11-18 11:26:30 -0500

    Strip quotes

--- home/workshop/config.js
+++ home/workshop/config.js
@@ -14,7 +14,7 @@ function initialize(workshop) {
   kubectl = spawnSync('kubectl', ['-n', namespace, 'get', 'harborproject', namespace, '-o', 'jsonpath="{.status.projectid}"']);
 
   if(kubectl.status == 0) {
-    let project_id = kubectl.stdout;
+    let project_id = kubectl.stdout.replaceAll("\"", "");
     console.log(`Got harbor project id: ${project_id}` );
     workshop.data_variable('harbor_project_id', project_id);
   } else {
