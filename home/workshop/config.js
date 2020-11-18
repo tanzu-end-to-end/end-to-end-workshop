0188fcac68f748412eee1758c60fa61cd1a8dee3
Chris DeLashmutt <cdelashmutt@pivotal.io>
2020-11-18 11:03:19 -0500

    Run kubectl in proper namespace

--- home/workshop/config.js
+++ home/workshop/config.js
@@ -10,7 +10,8 @@ function initialize(workshop) {
   
   workshop.data_variable('user_token', data.users[0].user.token);
 
-  kubectl = spawn('kubectl', ['get', 'harborproject', process.env['SESSION_NAMESPACE'], '-o', 'jsonpath="{.status.projectid}"']);
+  let namespace = process.env['SESSION_NAMESPACE']
+  kubectl = spawn('kubectl', ['-n', namespace, 'get', 'harborproject', namespace, '-o', 'jsonpath="{.status.projectid}"']);
 
   let kubectl_out = ""
   let kubectl_err = ""
