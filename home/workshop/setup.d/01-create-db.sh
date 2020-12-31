d31a8e47382e6bbebc439c017d798f93ce2e2e2e
cpage-pivotal <cpage@pivotal.io>
2020-12-31 10:50:39 -0600

    Update 01-create-db.sh

--- home/workshop/setup.d/01-create-db.sh
+++ home/workshop/setup.d/01-create-db.sh
@@ -2,9 +2,6 @@
 
 set -x
 
-echo "Hi ${MESSAGE}"
-echo $MESSAGE
-
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 helm upgrade -i petclinic-db bitnami/mysql --version 6.14.11 -f <(cat petclinic-db-values.yaml | envsubst)
