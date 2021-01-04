953b1853cb0701486342b0e7e43a22eb495c5729
cpage-pivotal <cpage@pivotal.io>
2021-01-04 08:37:41 -0600

    Support Installation of Tanzu Build Service

--- install/harbor/install-harbor.sh
+++ install/harbor/install-harbor.sh
@@ -5,4 +5,4 @@ helm repo update
 
 ytt -f harbor-helm-values.yaml -f $1 | helm template harbor/harbor --name-template harbor -f- > chart.yaml
 
-ytt -f harbor-dependencies.yaml -f $1 -f chart.yaml --file-mark 'chart.yaml:type=yaml-plain' | kapp deploy -a harbor -n harbor -f- --diff-changes --yes
+ytt -f harbor-dependencies.yaml -f $1 -f chart.yaml -f integrate-contour-overlay.yaml --file-mark 'chart.yaml:type=yaml-plain' | kapp deploy -a harbor -n harbor -f- --diff-changes --yes
