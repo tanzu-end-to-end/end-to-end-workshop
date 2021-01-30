75b81f540263a1826f5230fe10b699791cb6835a
cpage-pivotal <cpage@pivotal.io>
2021-01-30 16:19:57 -0600

    Git repo Initialization

---
+++ home/spring-webdb-config/README.md
@@ -0,0 +1,1 @@
+# spring-webdb-config
--- home/workshop/setup.d/04-gitops-repo.sh
+++ home/workshop/setup.d/04-gitops-repo.sh
@@ -5,6 +5,18 @@ pwd
 
 if [ $WORKSHOP_FILE == "workshop-tbs-gitops.yaml" ]
 then
+  cd spring-webdb-config
+  git init
+  git checkout -b main
+  git config user.name gitea_admin
+  git config user.email "gitea_admin@example.com"
+  git add .
+  git commit -a -m "Initial Commit"
+
+  export REPO_NAME=$SESSION_NAMESPACE-$RANDOM
+  git remote add origin https://gitea_admin:$GITEA_PASSWORD@gitea.contour.e2e.corby.cc/gitea_admin/$REPO_NAME.git
+  git push -u origin main
+
   argocd login argocd-cli.${INGRESS_DOMAIN} --username admin --password $ARGOCD_PASSWORD
-  argocd app create dev-${SESSION_NAMESPACE} --repo https://github.com/cpage-pivotal/spring-webdb-config --dest-namespace ${SESSION_NAMESPACE} --dest-server https://kubernetes.default.svc --path dev
+  argocd app create dev-${SESSION_NAMESPACE} --repo https://gitea.contour.e2e.corby.cc/gitea_admin/$REPO_NAME --dest-namespace ${SESSION_NAMESPACE} --dest-server https://kubernetes.default.svc --path dev
 fi
