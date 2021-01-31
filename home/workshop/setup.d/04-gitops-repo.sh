6490bbbdf5987a0d3a720832dfb841e260877238
cpage-pivotal <cpage@pivotal.io>
2021-01-31 12:28:10 -0600

    Git repo Initialization

--- home/workshop/setup.d/04-gitops-repo.sh
+++ home/workshop/setup.d/04-gitops-repo.sh
@@ -18,5 +18,5 @@ then
   git push -u origin main
 
   argocd login argocd-cli.${INGRESS_DOMAIN} --username admin --password $ARGOCD_PASSWORD
-  argocd app create dev-${SESSION_NAMESPACE} --repo https://gitea.contour.e2e.corby.cc/gitea_admin/$REPO_NAME --dest-namespace ${SESSION_NAMESPACE} --dest-server https://kubernetes.default.svc --path dev
+  argocd app create dev-${SESSION_NAMESPACE} --repo https://gitea.contour.e2e.corby.cc/gitea_admin/$REPO_NAME --dest-namespace ${SESSION_NAMESPACE} --dest-server https://kubernetes.default.svc --path dev --project ${SESSION_NAMESPACE}
 fi
