cf165037fd10b4a1e624a8d71bb41ec2c74c8857
cpage-pivotal <cpage@pivotal.io>
2021-02-01 09:54:17 -0600

    Git repo Initialization

--- home/workshop/setup.d/04-gitops-repo.sh
+++ home/workshop/setup.d/04-gitops-repo.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -x
+set +e
 
 if [ $WORKSHOP_FILE == "workshop-tbs-gitops.yaml" ]
 then
   export YTT_session=${SESSION_NAMESPACE}
   export YTT_ingress__domain=${INGRESS_DOMAIN}
-  ytt -f spring-webdb-config/dev/httpproxy.yaml -f values.yaml --data-values-env YTT > spring-webdb-config/dev/httpproxy.yaml
+  ytt -f spring-webdb-config/dev/httpproxy.yaml -f values.yaml --data-values-env YTT | tee spring-webdb-config/dev/httpproxy.yaml
 
   cd spring-webdb-config
   git init
@@ -20,5 +21,6 @@ then
   git push -u origin main
 
   argocd login argocd-cli.${INGRESS_DOMAIN} --username admin --password $ARGOCD_PASSWORD
+  argocd app get dev-${SESSION_NAMESPACE} && argocd app delete dev-${SESSION_NAMESPACE}
   argocd app create dev-${SESSION_NAMESPACE} --repo https://gitea.${INGRESS_DOMAIN}/gitea_admin/$REPO_NAME --dest-namespace ${SESSION_NAMESPACE} --dest-server https://kubernetes.default.svc --path dev --project ${SESSION_NAMESPACE}
 fi
