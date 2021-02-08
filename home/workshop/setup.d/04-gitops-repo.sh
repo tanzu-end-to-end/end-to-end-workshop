f19ac5714254616429a2f6ae43077a8f7617c359
cpage-pivotal <cpage@pivotal.io>
2021-02-08 11:46:17 -0600

    Customize deployment image for GitOps repo

--- home/workshop/setup.d/04-gitops-repo.sh
+++ home/workshop/setup.d/04-gitops-repo.sh
@@ -8,6 +8,8 @@ then
   export YTT_ingress__domain=${INGRESS_DOMAIN}
   ytt -f spring-webdb-config/dev/httpproxy.yaml -f values.yaml --data-values-env YTT | tee httpproxy.yaml
   mv httpproxy.yaml spring-webdb-config/dev/httpproxy.yaml
+  ytt -f spring-webdb-config/base/deployment.yaml -f values.yaml --data-values-env YTT | tee httpproxy.yaml
+  mv httpproxy.yaml spring-webdb-config/dev/httpproxy.yaml
 
   cd spring-webdb-config
   git init
@@ -22,7 +24,5 @@ then
   git push -u origin main
 
   argocd login argocd-cli.${INGRESS_DOMAIN} --username admin --password $ARGOCD_PASSWORD
-#  argocd app get dev-${SESSION_NAMESPACE} && argocd app delete dev-${SESSION_NAMESPACE}
-#  argocd app create dev-${SESSION_NAMESPACE} --repo https://gitea.${INGRESS_DOMAIN}/gitea_admin/$REPO_NAME --dest-namespace ${SESSION_NAMESPACE} --dest-server https://kubernetes.default.svc --path dev --project ${SESSION_NAMESPACE}
   argocd app set ${SESSION_NAMESPACE} --repo https://gitea.contour.e2e.corby.cc/gitea_admin/$REPO_NAME
 fi
