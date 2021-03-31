a294ad80a749ec010e201f5f92d0232f44bdf10d
cpage-pivotal <cpage@pivotal.io>
2021-03-31 14:54:56 -0500

    Implementing feedback on GitOps workshop

--- install/workshop/install-workshop.sh
+++ install/workshop/install-workshop.sh
@@ -1,3 +1,3 @@
 set -x
 
-ytt template -f ../../base -f $1 | kapp deploy -n default -a workshop -f- --diff-changes --yes
+ytt template -f ../../base -f $1 --ignore-unknown-comments | kapp deploy -n default -a workshop -f- --diff-changes --yes
