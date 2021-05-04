1a428e9767a88c93696bbc2217d3dd636bb7e23c
Chris DeLashmutt <cdelashmutt@pivotal.io>
2021-05-04 17:57:28 -0400

    Suppress error checking

--- home/workshop/setup.d/03-harbor-credentials.sh
+++ home/workshop/setup.d/03-harbor-credentials.sh
@@ -3,13 +3,13 @@ set -x
 
 docker login harbor.$INGRESS_DOMAIN -u admin -p $HARBOR_PASSWORD
 
-set +x
+set +e
 echo Checking for harbor-creds
 if ! k get secret harbor-creds 2> /dev/null ; then 
   echo No harbor-creds found, creating
-  set -x
+  set -e
   REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create harbor-creds --registry harbor.$INGRESS_DOMAIN --registry-user admin
 else
   echo Secret harbor-creds already exists, skiping creation
-  set -x
+  set -e
 fi
