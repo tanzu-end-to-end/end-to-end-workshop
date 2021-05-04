0e46a71b73107ba940532a284f0aa4a3c39f0402
Chris DeLashmutt <cdelashmutt@pivotal.io>
2021-05-04 17:54:09 -0400

    Adding checking for existing harbor-creds

--- home/workshop/setup.d/03-harbor-credentials.sh
+++ home/workshop/setup.d/03-harbor-credentials.sh
@@ -3,4 +3,13 @@ set -x
 
 docker login harbor.$INGRESS_DOMAIN -u admin -p $HARBOR_PASSWORD
 
-REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create harbor-creds --registry harbor.$INGRESS_DOMAIN --registry-user admin
+set +x
+echo Checking for harbor-creds
+if ! k get secret harbor-creds 2> /dev/null ; then 
+  echo No harbor-creds found, creating
+  set -x
+  REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create harbor-creds --registry harbor.$INGRESS_DOMAIN --registry-user admin
+else
+  echo Secret harbor-creds already exists, skiping creation
+  set -x
+fi
