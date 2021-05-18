d5488b0a40fbd87d90a261d6f2d8860d88e7ab5d
ubuntu <ubuntu@ubuntu-421.haas-421.pez.vmware.com>
2021-05-18 02:12:44 -0700

    EMEA env adaptions

--- artifactory/install.sh
+++ artifactory/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+kubectl create ns artifactory-oss
 helm repo add jfrog https://charts.jfrog.io
 helm repo update
 kubectl -n artifactory-oss apply -f bootstrap-config.yaml
--- install/kubeapps/install-kubeapps.sh
+++ install/kubeapps/install-kubeapps.sh
@@ -3,6 +3,6 @@ set -x
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-ytt -f kubeapps-helm-values.yaml -f $1 | helm template bitnami/kubeapps --version 4.0.4 --namespace kubeapps --name-template kubeapps -f- > chart.yaml
+ytt -f kubeapps-helm-values.yaml -f $1 | helm template bitnami/kubeapps --global.imageRegistry=harbor-repo.vmware.com/dockerhub-proxy-cachharbor-repo.vmware.com/dockerhub-proxy-cache  --version 4.0.4 --namespace kubeapps --name-template kubeapps -f- > chart.yaml
 
 ytt -f kubeapps-dependencies.yaml -f integrate-contour-overlay.yaml -f chart.yaml -f $1 --file-mark 'chart.yaml:type=yaml-plain' | kapp deploy -a kubeapps -n kubeapps -f- --diff-changes --yes
