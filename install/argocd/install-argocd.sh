8e380574fd6ac9acae923d5935e941632e8b1710
cpage-pivotal <cpage@pivotal.io>
2021-08-08 16:36:32 -0500

    Set up HTTPProxy support

--- install/argocd/install-argocd.sh
+++ install/argocd/install-argocd.sh
@@ -1,3 +1,3 @@
 set -x
 
-ytt -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml -f integrate-contour-overlay.yaml --file-mark 'install.yaml:type=yaml-plain' -f argocd-dependencies.yaml -f $1 | kapp deploy -n argocd -a argocd -f- --diff-changes --yes
+ytt -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml -f integrate-contour-overlay.yaml --ignore-unknown-comments -f argocd-dependencies.yaml -f $1 | kapp deploy -n argocd -a argocd -f- --diff-changes --yes
