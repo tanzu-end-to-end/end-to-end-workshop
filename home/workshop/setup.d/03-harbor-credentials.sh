83e1cd4fff2787a4ce88038eaa90c7d383c6fc8c
Timo Salm <52704922+tsalm-pivotal@users.noreply.github.com>
2022-06-03 14:25:04 +0200

    Update 03-harbor-credentials.sh

--- home/workshop/setup.d/03-harbor-credentials.sh
+++ home/workshop/setup.d/03-harbor-credentials.sh
@@ -2,14 +2,4 @@
 set -x
 
 docker login harbor.$TAP_INGRESS_DOMAIN -u admin -p $HARBOR_PASSWORD
-
-set +e
-echo Checking for harbor-creds
-if ! kubectl get secret harbor-creds ; then 
-  echo No harbor-creds found, creating
-  set -e
-  REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create harbor-creds --registry harbor.$TAP_INGRESS_DOMAIN --registry-user admin
-else
-  echo Secret harbor-creds already exists, skiping creation
-  set -e
-fi
+REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create harbor-creds --registry harbor.$TAP_INGRESS_DOMAIN --registry-user admin
